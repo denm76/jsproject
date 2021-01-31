70496957e1570f9dab8ef037529bc8993d68f09c
DenM76 <DenM76@gmail.com>
2021-01-31 14:11:14 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const obj={
+    name:"John",
+    age:12
+};
+
+let arr= ['as.png', obj, 123];
+
+console.log(arr[1]['name']);
+
+console.log(prompt('Choose your destiny!'));
+
+console.log(typeof(NaN));
+
